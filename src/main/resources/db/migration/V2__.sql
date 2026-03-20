2916f84231e9e1679c8a92cf552e94a5c464bf7d
ti60 <ti60@student.le.ac.uk>
2026-03-20 16:19:46 +0000

    fix: remove duplicate root level backend files and add .DS_Store to gitignore

--- src/main/resources/db/migration/V2__.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE population_history
-DROP
-COLUMN population;
-
-ALTER TABLE population_history
-    ADD population BIGINT NOT NULL;
--- src/main/resources/db/migration/V3__.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE population_history
-    ADD CONSTRAINT unique_country_id_year UNIQUE (country_id, year);
